d1fd374ff9107b30b4764008943734289fe7c73a
ALO <36780370+Osteribo@users.noreply.github.com>
2020-07-03 07:14:35 -0700

    Update: function + visual finished, Registration

--- users/views.py
+++ users/views.py
@@ -1,19 +1,21 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import UserRegisterForm
 # Create your views here.
 
 
 def register(request):
     # new users registration already provieded by django
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         # is imput data valid
         if form.is_valid():
+            form.save()
             username = form.cleaned_data.get('username')
             # flash message saying it was succesful
             messages.success(request, f'Account Created For {username}!')
             return redirect('blog-home')
     else:
-        form = UserCreationForm()
+        form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
