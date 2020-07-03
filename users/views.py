53203c7b62069ff8631cb7dbd92bd60d80ce25a7
ALO <36780370+Osteribo@users.noreply.github.com>
2020-07-02 22:58:56 -0700

    Update: Flash message and validation error added

--- users/views.py
+++ users/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 # Create your views here.
+
 
 def register(request):
     # new users registration already provieded by django
-    form = UserCreationForm()
-    return render(request, 'users/register.html', {'from': form})
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        # is imput data valid
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            # flash message saying it was succesful
+            messages.success(request, f'Account Created For {username}!')
+            return redirect('blog-home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'users/register.html', {'form': form})
