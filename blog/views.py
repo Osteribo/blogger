8374cad665bd799d76a4d83bbf8c3e1f8892dced
ALO <36780370+Osteribo@users.noreply.github.com>
2020-07-08 18:32:26 -0700

    Update: added detail page for blogs and links

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView, DetailView
 
 
 posts = [
@@ -29,6 +30,23 @@ def home(request):
     return render(request, 'blog/home.html', context)
 
 
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'   #  <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    # orders the blogs newest to oldest
+    ordering = ['-date_posted']
+   
+
+
+class PostDetailView(DetailView):
+    model = Post
+    
+
+
+
+
+
 def about(request):
-    return render(request, 'blog/about.html', {'title': 'About'} )
+    return render(request, 'blog/about.html', {'title': 'About'})
 
