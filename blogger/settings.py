4c809853c33c50e081517b35e0d83726eb97c290
ALO <36780370+Osteribo@users.noreply.github.com>
2020-07-20 21:30:44 -0700

    Update: added in settings export

--- blogger/settings.py
+++ blogger/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY2')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["django-blogger-osteribo.herokuapp.com"]
+ALLOWED_HOSTS = ["django-blogger-osteribo.herokuapp.com", "127.0.0.1"]
 
 
 # Application definition
@@ -154,3 +154,5 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+django_heroku.settings(locals())
