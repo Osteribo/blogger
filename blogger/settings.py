6a705aa25cdcf334777e58b9b05cbe1fffacec34
ALO <36780370+Osteribo@users.noreply.github.com>
2020-07-20 21:19:25 -0700

    Fix: made turned allowed hosts to string value

--- blogger/settings.py
+++ blogger/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY2')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [django-blogger-osteribo.herokuapp.com]
+ALLOWED_HOSTS = ["django-blogger-osteribo.herokuapp.com"]
 
 
 # Application definition
